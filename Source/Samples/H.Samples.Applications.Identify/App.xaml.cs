dd47908184205af6f780924f3c81acd454260800
haijun.li <haijun.li@pasasoft.com>
2025-04-11 13:33:39 +0800

    Identify+

---
+++ Source/Samples/H.Samples.Applications.Identify/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using H.Extensions.ApplicationBase;
+using H.Themes.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace H.Samples.Applications.Identify;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : ApplicationBase
+{
+    protected override void ConfigureServices(IServiceCollection services)
+    {
+        //  Do ：使用默认配置
+        services.AddApplicationServices();
+
+        services.AddDefaultIdentify();
+
+    }
+
+    protected override void Configure(IApplicationBuilder app)
+    {
+        //  Do ：启用默认配置，会把这些配置添加到系统设置页面中
+        app.UseApplicationOptions();
+        app.UseDefaultIdentifyOptions();
+        app.UseIdentifyOptions();
+    }
+
+    protected override Window CreateMainWindow(StartupEventArgs e)
+    {
+        return new MainWindow();
+    }
+}
